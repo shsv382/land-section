014a605a14805331092bcc53dcf4f293bc155b63
shsv382 <shsv382@gmail.com>
2017-10-25 21:29:51 +0400

    Modifyed front-end folder

---
+++ front-end/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$(".head-banner > h3").fadeIn(600, function(){
+		$(".head-banner > h4").fadeIn(600, function(){
+			$(".head-banner > h1").fadeIn(600);
+		});
+	});
+});
